3d8fa9c255ddeec0dcf286f183c5e0abca45132d
soni653 <dave.family653@outlook.com>
2020-01-23 19:24:02 -0500

    Final Update

--- index.js
+++ index.js
@@ -39,9 +39,29 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+ Person.prototype.eat = function(someFood){
+   if(this.stomach.length < 10)
+     {
+   this.stomach.push(someFood) 
+     }
+   
+  }
+  Person.prototype.poop =function()
+  {
+   this.stomach = [];
+
+    }
+  
+Person.prototype.toString = function(){
+  return `${this.name},${this.age}`;
+}
+
+
 
 /*
   TASK 2
@@ -57,9 +77,32 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model,milesPerGallon) {
+  this.tank = 0;
+  this.odometer = 0;
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
 }
+Car.prototype.fill = function(gallons){
+ return this.tank += gallons;
+}
+Car.prototype.drive = function(distance){
+  //check tank - if distance / milesPerGallon >= tank
+  if (distance / this.milesPerGallon >= this.tank) {
+    // increase odometer until tank empty
+    this.odometer = this.odometer + this.tank * this.milesPerGallon;
+    //set tank value to 0
+    this.tank = 0;
+    //return run out of gas @ x miles
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  } else {
+    this.tank -= distance / this.milesPerGallon;
+    this.odometer += distance;
+  }
+}
+
+
+
 
 /*
   TASK 3
@@ -68,18 +111,25 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name,age,favoriteToy) {
+  Person.call(this,name,age);
+  this.favoriteToy = favoriteToy;
 
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. "this" is interchangable for the object that we're living in.
+  2. "this" refers to whatever comes before the dot(.).
+  3. "this" In a function defination "this" refers to the 'owner' of the function.
+  4. Every javascript function while executing has a reference to its current execution context,
+     called "this".execution context means here is how the function is called.
 */
 
 
